1dc9d2a028a2d5fddb432ce5559458648ed4a40e
Mutia Dishon Wambua <48084263+dishonwambu@users.noreply.github.com>
2019-07-21 17:11:21 +0300

    Add files via upload

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,35 @@
+
+package swaptwonumbers;
+
+import java.util.Scanner;
+
+ 
+
+public class SwapTwoNumbers {
+
+    public static void main(String[] args) {
+
+        // TODO Auto-generated method stub
+
+        int x, y, temp;
+
+        System.out.println("Enter x and y");
+
+        Scanner in = new Scanner(System.in);
+
+        x = in.nextInt();
+
+        y = in.nextInt();
+
+        System.out.println("Before Swapping x = " + x +": y = "+ y);
+
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.println("After Swapping x = " + x +": y = "+ y);
+
+         
+
+    }
+}
+
